bfd8ad300b77ac10ae7e1fa1eaf0c6bedf31d4ad
sty00A4 <99866356+sty00A4@users.noreply.github.com>
2023-03-30 16:21:47 +0200

    more sdl2 features

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 #![allow(unused)]
+pub extern crate sdl2;
 pub mod vector;
 pub mod color;
 pub mod canvas;
 pub mod app;
-pub use sdl2::{event, keyboard, sys::{rand, random}};
+pub use sdl2::{event, keyboard, sys::{rand, random}, timer};
 
 #[cfg(test)]
 mod tests {
